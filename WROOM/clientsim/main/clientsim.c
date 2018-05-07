23bce2a297435fdb7e786b4de808c02dc7802b57
Cosmin Vrinceanu <cosmn.vrinceanu@gmail.com>
2018-05-07 18:07:34 +0200

    progetto vuoto e pronto per lavorarci

--- WROOM/clientsim/main/clientsim.c
+++ WROOM/clientsim/main/clientsim.c
@@ -15,33 +15,33 @@
 /* Can run 'make menuconfig' to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define BLINK_GPIO 2
+//#define BLINK_GPIO 2
 
-void blink_task(void *pvParameter)
-{
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
-    gpio_pad_select_gpio(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
-        /* Blink off (output low) */
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        printf("LED blinker\n");
-        fflush(stdout);
-    }
-}
+//void blink_task(void *pvParameter)
+//{
+//    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+//       muxed to GPIO on reset already, but some default to other
+//       functions and need to be switched to GPIO. Consult the
+//       Technical Reference for a list of pads and their default
+//       functions.)
+//    */
+//    gpio_pad_select_gpio(BLINK_GPIO);
+//    /* Set the GPIO as a push/pull output */
+//    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+//    while(1) {
+//        /* Blink off (output low) */
+//        gpio_set_level(BLINK_GPIO, 0);
+//        vTaskDelay(1000 / portTICK_PERIOD_MS);
+//        /* Blink on (output high) */
+//        gpio_set_level(BLINK_GPIO, 1);
+//        vTaskDelay(1000 / portTICK_PERIOD_MS);
+//        printf("LED blinker\n");
+//        fflush(stdout);
+//    }
+//}
 
 void app_main()
 {
-	printf("LED blinker\n");
-    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+	printf("WinMAS avviato\n");
+//    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
 }
