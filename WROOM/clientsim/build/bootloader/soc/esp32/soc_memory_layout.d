ede927f21dafeeb2b49d1d25fd41eaaf61678aa0
Cosmin Vrinceanu <cosmn.vrinceanu@gmail.com>
2018-05-12 21:34:14 +0200

    eliminata cartella build

--- WROOM/clientsim/build/bootloader/soc/esp32/soc_memory_layout.d
+++
@@ -1,3 +0,0 @@
-esp32/soc_memory_layout.o: \
- /home/z/esp/esp-idf/components/soc/esp32/soc_memory_layout.c
-/home/z/esp/esp-idf/components/soc/esp32/soc_memory_layout.c:
--- WROOM/clientsim/build/expat/library/xmltok_impl.d
+++
@@ -1,3 +0,0 @@
-library/xmltok_impl.o: \
- /home/z/esp/esp-idf/components/expat/library/xmltok_impl.c
-/home/z/esp/esp-idf/components/expat/library/xmltok_impl.c:
