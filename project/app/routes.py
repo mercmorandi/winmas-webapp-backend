87435057865d12c455dfa0a3c559bd8c8cbc65ae
Alberto <benincasa.alberto@gmail.com>
2020-05-01 16:25:59 +0200

    get_valid_packets functions work

--- project/app/routes.py
+++ project/app/routes.py
@@ -24,7 +24,44 @@ def database_info():
 @app.route('/add_req', methods=['POST'])
 def add_req():
     data = request.json
-    print(data)
-    return "ciao", 200, {
-            'ContentType': 'text/plain'
-        }
+    esp_data, devices_id = [], []
+
+    if data['device_id'] not in devices_id:
+        devices_id.append(data['device_id'])
+        esp_data.append(data)
+
+        #TODO: len(esp_data) is never 3: we need some global variables and then a thread that call get_valid_packet
+        #if len(esp_data) is 3:
+        #    v = get_valid_packets(esp_data)
+        #    esp_data, devices_id = [], []
+
+def get_valid_packets(data):
+    valid_packets = []
+
+    for datum in data[0]['data']:
+
+        valid_packet = {}
+        valid_packet['MAC'] = datum['source']
+        valid_packet['SSID'] = datum['ssid']
+        valid_packet['time'] = datum['timestamp']
+        valid_packet['signals'] = []
+
+        valid_packet['signals'].append({data[0]['device_id']: datum['signal_strength_wroom']})
+
+        cont = 1
+
+        timestamp = datum['timestamp']
+        MAC = datum['source']
+
+        for esp in data[1:]:
+            packet = [p for p in esp['data'] if p['timestamp'] is timestamp and p['source'] is MAC][0]
+
+            if not packet: 
+                break
+            
+            valid_packet['signals'].append({esp['device_id']: packet['signal_strength_wroom']})
+            cont += 1
+            if cont is len(data):
+                valid_packets.append(valid_packet)
+
+    return valid_packets
