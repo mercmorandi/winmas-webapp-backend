60f49408b35a8cf80d9e4376f8b1aca9889b1c48
mrk-stss <merkmorad@gmail.com>
2020-06-11 18:22:02 +0200

    trilaterator impl (not yet tested)

--- app/tasks.py
+++ app/tasks.py
@@ -26,14 +26,14 @@ def filter_task():
     logger.info(str(db.session))
     q = (
         db.session.query(Probe.hash, func.count(Probe.hash).label("probe_count"))
-        .group_by(Probe.hash)
-        .subquery()
+            .group_by(Probe.hash)
+            .subquery()
     )
     q2 = (
         Probe.query.join(q, Probe.hash == q.c.HASH)
-        .filter(Probe.status == "unchecked")
-        .filter(q.c.probe_count == app.config["NUMESP"])
-        .order_by(Probe.timestamp.desc())
+            .filter(Probe.status == "unchecked")
+            .filter(q.c.probe_count == app.config["NUMESP"])
+            .order_by(Probe.timestamp.desc())
     )
     logger.info(q2.all())
     # TODO scartare probes dello stesso device ricevute piu volte dallo stesso esp nello stesso minuto
@@ -58,7 +58,52 @@ def filter_task():
     return "done"
 
 
-@celery.task(name="triangular_task")
-def triangular_task():
+@celery.task(name="trilateration_task")
+def trilateration_task():
     # to_do = db.session.query(Probe).filter(Probe.status == 'pending')
+
     return "triangular task"
+
+
+def trilaterator(rssi_dict):
+    data_dict = {
+        device_id: (
+            compute_distance(rssi),
+            app.config["X" + device_id[-1:]],
+            app.config["Y" + device_id[-1:]],
+        )
+        for device_id, rssi in rssi_dict.items()
+    }
+    data_list = list(data_dict.values())
+    v_a = (
+                  (data_list[2][1] ^ 2 - data_list[1][1] ^ 2)
+                  + (data_list[2][2] ^ 2 - data_list[1][2] ^ 2)
+                  + (data_list[1][0] ^ 2 - data_list[2][0] ^ 2)
+          ) / 2
+
+    v_b = (
+                  (data_list[0][1] ^ 2 - data_list[1][1] ^ 2)
+                  + (data_list[0][2] ^ 2 - data_list[1][2] ^ 2)
+                  + (data_list[1][0] ^ 2 - data_list[0][1] ^ 2)
+          ) / 2
+
+    y = (
+                v_b * (data_list[1][1] - data_list[2][1])
+                - v_a * (data_list[1][1] - data_list[0][1])
+        ) / (
+                (data_list[0][2] - data_list[1][2]) * (data_list[1][1] - data_list[2][1])
+                - (data_list[2][2] - data_list[1][2]) * (data_list[1][1] - data_list[2][1])
+        )
+
+    x = (y * (data_list[0][2] - data_list[1][2]) - v_b) / (
+            data_list[1][1] - data_list[2][1]
+    )
+
+    return x, y
+
+
+def compute_distance(rssi):
+    distance = 10 ^ (
+            (app.config["ESP_MES_POWER"] - rssi) / 10 * app.config["ENV_FACTOR"]
+    )
+    return distance
