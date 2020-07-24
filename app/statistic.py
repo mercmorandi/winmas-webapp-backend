f8b7f1acf12a9a88f4018173214388aeffd32648
mrk-stss <merkmorad@gmail.com>
2020-07-24 19:09:28 +0200

    fixed duplicated location inside same minute in stats

--- app/statistic.py
+++ app/statistic.py
@@ -5,24 +5,43 @@ from app import db
 from app.models.locations import Location
 
 
+class StatsDto:
+    def __init__(self, minutes, ndevices):
+        self.minutes = minutes
+        self.nDevice = ndevices
+
+    def __str__(self):
+        return self.minutes + ": " + self.nDevice
+
+
 def serve_stats(start_date):
     start_date_dt = datetime.fromtimestamp(int(start_date))
     start_date_dt = start_date_dt.replace(second=0, microsecond=0)
     res = {}
     for row in range(start_date_dt.minute, start_date_dt.minute + 5):
         res[row] = 0
-    qs = (
+    qs1 = (
+        db.session.query(Location.id)
+        .filter(Location.insertion_date >= start_date_dt)
+        .filter(Location.insertion_date < start_date_dt + timedelta(minutes=5))
+        .distinct(Location.mac_id)
+        .subquery()
+    )
+
+    qs2 = (
         db.session.query(
             extract("minutes", Location.insertion_date).label("m"), func.count("m")
         )
-            .filter(Location.insertion_date >= start_date_dt)
-            .filter(Location.insertion_date < start_date_dt + timedelta(minutes=5))
-            .group_by("m")
-            .order_by("m")
+        .join(qs1, Location.id == qs1.c.id)
+        .group_by("m")
+        .order_by("m")
     )
 
-    data = {int(line.m): line[1] for line in qs.all()}  # (minute, count)
+    data = {int(line.m): line[1] for line in qs2.all()}  # (minute, count)
     res.update(data)
 
     db.session.close()
+
+    dtoRes = [StatsDto(k, v) for k, v in data.items()]
+    print(str(dtoRes))
     return res
