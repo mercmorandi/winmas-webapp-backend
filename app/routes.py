17d9801c82f196b55148d51427fac70de95c59d0
mrk-stss <merkmorad@gmail.com>
2020-07-28 18:29:36 +0200

    added probe_parser method to clean controller (need testing)

--- app/routes.py
+++ app/routes.py
@@ -1,37 +1,16 @@
-import time
-
 from flask import current_app as app
 from flask_cors import cross_origin
-
-from . import db_connection
 from flask import request
 from sqlalchemy.exc import IntegrityError
 
-from .models import probes
-from app.tasks import trilaterable_check_task
-
-from hashlib import md5
-
 from app import db, tasks, statistic, positions
-from . import tasks
+from app.models import probes
 
 
 @app.route("/", methods=["GET"])
 def index():
-    tasks.add.delay(11, 22)
+    # tasks.add.delay(11, 22)
     return "hello world"
-
-
-@app.route("/info", methods=["GET"])
-def database_info():
-    """
-        Returns the all data stored in the database.
-    """
-    try:
-        db = db_connection.DBConnection()
-    except IOError:
-        return "Database connection not possible", 504, {"ContentType": "text/plain"}
-    return db.get_database_info(), 200, {"ContentType": "application/json"}
 
 
 @app.route("/test_task", methods=["GET"])
@@ -40,81 +19,24 @@ def test_task():
     return "test task done", 200
 
 
-@app.route("/add_req_test", methods=["POST"])
-def add_req_test():
-    if not request.json:
-        return "no data", 400
-
-    print(str(request.json))
-    req = request.json
-    device_id = req["device_id"]
-    on_since = int(req["on_since"])
-    probe = req["probe"]
-
-    ts = int(round(time.time() * 1000)) - (on_since - int(probe["timestamp"]))
-    minutes_ts = int(ts / 1000 / 60)
-    to_encode = (
-        probe["destination"]
-        + ""
-        + probe["source"]
-        + ""
-        + str(minutes_ts)
-        + ""
-        + probe["seq_number"]
-    )
-    h = md5(to_encode.encode("utf-8")).hexdigest()
-    print("HASSSSSSSSH: " + str(h))
-    return "ok", 200
-
-
 @app.route("/add_req", methods=["POST"])
 def add_req():
     if not request.json:
         return "no data", 400
 
     print(str(request.json))
-    req = request.json
-    device_id = req["device_id"]
-    on_since = int(req["on_since"])
-    probe = req["probe"]
-
-    ts = int(round(time.time() * 1000)) - (on_since - int(probe["timestamp"]))
-    minutes_ts = int(ts / 1000 / 60)
-    to_encode = (
-        probe["destination"]
-        + ""
-        + probe["source"]
-        + ""
-        + str(minutes_ts)
-        + ""
-        + probe["seq_number"]
-    )
-    h = md5(to_encode.encode("utf-8")).hexdigest()
-    print("HASSSSSSSSH: " + str(h))
-    probe = probes.Probe(
-        destination=probe["destination"],
-        source=probe["source"],
-        bssid=probe["bssid"],
-        ssid=probe["ssid"],
-        signal_strength_wroom=probe["signal_strength_wroom"],
-        signal_strength_rt=probe["signal_strength_rt"],
-        hash=str(h),
-        timestamp=ts,
-        seqnum=probe["seq_number"],
-        esp_id=device_id,
-        status="unchecked",
-    )
+    probe = probes.probe_parser(request.json)
 
     db.session.add(probe)
     try:
         db.session.commit()
         db.session.close()
-        tasks.trilaterable_check_task.delay(str(h))
+        tasks.trilaterable_check_task.delay(str(probe.hash))
         return "ok", 200
     except IntegrityError:
         db.session.rollback()
         return (
-            "hash: " + str(h) + " and esp_id: " + probe.esp_id + " already exists",
+            "hash: " + str(probe.hash) + " and esp_id: " + probe.esp_id + " already exists",
             409,
         )
         # error, there already is a probe using this hash and esp_id
