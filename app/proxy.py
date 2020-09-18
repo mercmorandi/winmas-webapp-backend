bd471a6165fbb4c16316370ad10ee0779b29e821
mrk-stss <merkmorad@gmail.com>
2020-09-18 17:57:54 +0200

    fixed close proxy

--- app/proxy.py
+++ app/proxy.py
@@ -4,21 +4,14 @@ import types
 import signal
 import requests
 
-from flask_socketio import send
-
-from app import tasks, socketio
+from app import tasks, celery
 
 
-@socketio.on("new-message")
-def notify_message(message):
-    print("into notify")
-    send(message)
+def close_proxy_server(host, port):
 
-
-@socketio.on("proxy_status")
-def notify_proxy_status(status):
-    print("into notify")
-    send(status)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        s.sendall(b"close")
 
 
 class Proxy:
@@ -41,18 +34,28 @@ class Proxy:
         signal.signal(signal.SIGTERM, self.receiveSignal)
         self.lsock.bind((host, int(port)))
         self.lsock.listen()
-        requests.post("http://backend:5000/proxy_status", json={"status": "on"})
+        task_id = celery.current_task.request.id
+        print("task_id: " + str(task_id))
+        requests.post(
+            "http://backend:5000/proxy_status",
+            json={"status": "on", "task_id": str(task_id)},
+        )
         print("listening on", (host, port))
         self.lsock.setblocking(False)
         self.sel.register(self.lsock, selectors.EVENT_READ, data=None)
         while True:
             events = self.sel.select(timeout=None)
+            status = None
             for key, mask in events:
                 if key.data is None:
                     self.accept_wrapper(key.fileobj)
                 else:
-                    # print('mask: '+str(mask))
-                    self.service_connection(key, mask)
+                    status = self.service_connection(key, mask)
+                    print("status: " + str(status))
+            if status:
+                break
+        self.lsock.close()
+        requests.post("http://backend:5000/proxy_status", json={"status": "off"})
 
     def accept_wrapper(self, sock):
         conn, addr = sock.accept()  # Should be ready to read
@@ -74,8 +77,15 @@ class Proxy:
                 print("recv ok: " + str(recv_data))
             except Exception as e:
                 print("errore timeout: " + e)
+
             if recv_data:
                 data.inb += recv_data
+                print("data inb: " + str(data.inb))
+            elif data.inb == b"close":
+                data.inb = data.inb[len(str(data.inb)) :]
+                self.sel.unregister(sock)
+                sock.close()
+                return "close"
             else:
                 print("all data red")
                 data.outb = data.inb
